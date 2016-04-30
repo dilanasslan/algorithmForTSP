83fba1d616f30a5535d023093e0c16d6e73b276c
rabiaulusoy <rabiaulusoy123@gmail.com>
2016-05-01 01:21:20 +0300

    Update algorithm.c

--- algorithm.c
+++ algorithm.c
@@ -1,18 +1,54 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct node{
+    int id,x,y;
+    struct node *next;
+};
+struct node *head = NULL;
+
+void createLinkedList(struct node **firstPtr,int id,int x,int y){
+	struct node* new_node = (struct node*) malloc(sizeof(struct node));
+	struct node *last = *firstPtr;  
+		new_node->id  = id;
+		new_node->x  = x;
+		new_node->y  = y;
+		new_node->next = NULL;
+			if (*firstPtr == NULL){
+				*firstPtr = new_node;
+				return;
+			}
+		while (last->next != NULL)
+			last = last->next;
+
+	last->next = new_node;
+	return;
+}
+
+void printLinkedList(struct node *firstPtr){
+	struct node *current = firstPtr;
+		while(current != NULL){
+			printf("%d %d %d\n", current->id, current->x, current->y);
+			current=current->next;
+		}
+}
+	
+void readFileCreatLinkedList(){
+	FILE *file = fopen("/home/rabus/input.txt", "r");
+	int c,x,y, id;
+		do {
+			c = fscanf(file,"%d%d%d",&id,&x,&y);
+			if (c != EOF)
+			//printf("%d %d %d\n", id, x, y);
+			createLinkedList(&head,id,x,y);
+		} while (c != EOF);
+	fclose(file);
+}
+
 int main(void){
 	
-FILE *file = fopen("/home/rabus/Masaüstü/input.txt", "r");
-
-   char birkelime[100];
-   int c;
-   do {
-      c = fscanf(file,"%s",birkelime); /* dosyadan bir kelime oku... */
-      if (c != EOF)
-         printf("%s\n",birkelime);    /* ekrana yaz...            */
-   } while (c != EOF);             /* ta ki EOF olana dek..      */
-   fclose(file);                    /* dosyayı kapa       */
-   
+	readFileCreatLinkedList();
+	printLinkedList(head);
+	
     return 0;
 }
